095061a0648545a5e0120cf22093b8b6cb5fa1e6
Heather Arnold <harnold4882@gmail.com>
2017-10-06 14:24:39 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,5 +8,5 @@ puts  "   |   |   "
 puts  "-----------"
 puts  "   |   |   "
 end
-def display_board(board)# Define display_board that accepts a board and prints
+# Define display_board that accepts a board and prints
 # out the current state.
